146101d2909c794591c159295db866669f5d5a00
Brian Peiris <brianpeiris@gmail.com>
2019-02-10 16:17:55 -0800

    tweak start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 scriptDirectory=$(dirname "$0")
 scriptDirectory=$(realpath "$scriptDirectory")
 rootDirectory="$scriptDirectory/.."
+cd "$rootDirectory";
 for library in $(ls libraries); do
 	libraryDirectory="$rootDirectory/libraries/$library"
 	npx webpack "$libraryDirectory/src/main.js" -o "$libraryDirectory/dist/main.js" --mode development -w &
