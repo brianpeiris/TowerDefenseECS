f7cdb47e3a85a15f657164762d8204cf5fcca922
Brian Peiris <brianpeiris@gmail.com>
2019-02-14 04:54:25 -0800

    tweak build and deploy scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,5 @@ rootDirectory="$scriptDirectory/.."
 cd "$rootDirectory";
 for library in $(ls libraries); do
 	libraryDirectory="$rootDirectory/libraries/$library"
-	npx webpack "$libraryDirectory/src/main.js" -o "$libraryDirectory/dist/main.js" --mode production &
+	npx webpack "$libraryDirectory/src/main.js" -o "$libraryDirectory/dist/main.js" --mode production
 done
-trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
-wait
